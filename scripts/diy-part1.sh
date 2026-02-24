5d9c8a5db77431fa60c7bbee82803185c370b662
HAS <237227757@qq.com>
2026-02-24 12:57:29 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-#
-# 版权所有 (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# 这是一个自由软件，遵循 MIT 许可证。
-# 更多信息请见 /LICENSE 文件。
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# 文件名: diy-part1.sh
-# 描述: OpenWrt DIY 脚本第一部分 (更新 feeds 之前)
+# diy-part1.sh 或 diy-part2.sh
 
+cd $OPENWRT_DIR  # 假设 workflow 已 cd 到 openwrt，或用绝对路径 /workdir/openwrt
 
+# Patch mtwifi-cfg Makefile：移除对 l1dat 和 l1util 的安装
+sed -i '/l1dat/d' package/mtwifi-cfg/Makefile
+sed -i '/l1util/d' package/mtwifi-cfg/Makefile
 
+# 或者更精确，只注释或删除 INSTALL_BIN 行（如果 sed 不够准，可用 patch 文件）
+# 推荐：用 patch 方式更可靠
+cat > mtwifi-cfg-no-l1.patch << 'EOF'
+--- a/package/mtwifi-cfg/Makefile
++++ b/package/mtwifi-cfg/Makefile
+@@ -XX,8 +XX,6 @@ define Package/mtwifi-cfg/install
+ 	$(INSTALL_DIR) $(1)/sbin
+ 	# ... 其他行
+-	$(INSTALL_BIN) $(PKG_BUILD_DIR)/sbin/l1dat $(1)/sbin/l1dat
+-	$(INSTALL_BIN) $(PKG_BUILD_DIR)/sbin/l1util $(1)/sbin/l1util
+ 	# ... 其他行
+ endef
+EOF
+
+# 应用 patch（忽略行号失败）
+patch -p1 -N -r - --no-backup-if-mismatch < mtwifi-cfg-no-l1.patch || true
+rm -f mtwifi-cfg-no-l1.patch
